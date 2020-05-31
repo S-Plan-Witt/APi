709e2761030c9ba2380f8ce0137a4e4eeeb44042
Nils Witt <nils@nils-witt.de>
2020-05-31 09:25:54 +0200

    Enabled Push notifications
    Signed-off-by: Nils Witt <nils@nils-witt.de>

--- src/router/replacementLessonRouter.ts
+++ src/router/replacementLessonRouter.ts
@@ -20,27 +20,7 @@ export let router = express.Router();
  * @security JWT
  */
 router.post('/', async function (req,res){
-    /*
-    let lessons: any = [];
-    let allLessons: Lesson[] = await TimeTable.getAllLessons();
-    for (let i = 0; i < allLessons.length; i++) {
-        let lesson = allLessons[i];
-        if(!lessons.hasOwnProperty(lesson.course.grade)){
-            lessons[lesson.course.grade] = [];
-        }
-        if(!lessons[lesson.course.grade].hasOwnProperty(lesson.course.subject)){
-            lessons[lesson.course.grade][lesson.course.subject] = [];
-        }
-        if(!lessons[lesson.course.grade][lesson.course.subject].hasOwnProperty(lesson.course.group)){
-            lessons[lesson.course.grade][lesson.course.subject][lesson.course.group] = [];
-        }
-        if(!lessons[lesson.course.grade][lesson.course.subject][lesson.course.group].hasOwnProperty(lesson.day)){
-            lessons[lesson.course.grade][lesson.course.subject][lesson.course.group][lesson.day] = [];
-        }
-        lessons[lesson.course.grade][lesson.course.subject][lesson.course.group][lesson.day][lesson.lesson] = lesson;
-    }
 
-     */
     for(let i= 0; i < req.body.length;i++){
         let postDataSet: any = req.body[i];
 
@@ -66,34 +46,24 @@ router.post('/', async function (req,res){
             }
         }
         try {
-            assert(replacementLesson != null, "RL null")
+            assert(replacementLesson != null, "RL null");
             let status = await ReplacementLessons.add(replacementLesson);
-            console.log(status)
-/*
+
             let devices = await User.getStudentDevicesByCourse(replacementLesson.course);
-            //devices = devices.concat(await user.getDevicesByUsername("bad"));
-            console.log(status)
-            let pushNotifis = new PushNotifications();
-
-             */
-
-            /*
+            console.log(status + ":" + JSON.stringify(replacementLesson))
+            let pushNotifications = new PushNotifications();
             if(status === "added"){
-                pushNotifis.sendBulk(devices,"Hinzugefügt: " + replacementLesson.course.subject," Stunde: " + replacementLesson.lesson + " Info: " + replacementLesson.info + " Datum: " + replacementLesson.date);
+                pushNotifications.sendBulk(devices,"Hinzugefügt: " + replacementLesson.course.subject," Stunde: " + replacementLesson.lesson.lessonNumber + " Info: " + replacementLesson.info + " Datum: " + replacementLesson.date);
             }else if(status === "updated"){
-                pushNotifis.sendBulk(devices,"Aktualisiert: " + replacementLesson.course.subject," Stunde: " + replacementLesson.lesson + " Info: " + replacementLesson.info + " Datum: " + replacementLesson.date);
+                pushNotifications.sendBulk(devices,"Aktualisiert: " + replacementLesson.course.subject," Stunde: " + replacementLesson.lesson.lessonNumber + " Info: " + replacementLesson.info + " Datum: " + replacementLesson.date);
             }
-            */
-
 
         } catch(e){
-            console.log(e);
             logger.log({
                 level: 'warn',
                 label: 'ReplacementLessons',
                 message: 'Error while processing post ' + e + '; at: ' + JSON.stringify(postDataSet)
             });
-            //add handler
         }
     }
     res.sendStatus(200);
@@ -129,8 +99,13 @@ router.get('/', async function (req,res){
 router.get('/date/:date', async function (req,res){
     try {
         let date = req.params.date;
-        let result = await ReplacementLessons.getByDate(date);
-        await res.json(result);
+        let data: ReplacementLesson[] = await ReplacementLessons.getByDate(date);
+        let response: any[] = [];
+        data.forEach((replacementLesson: any) => {
+            let dataset = {id: replacementLesson.id, courseId: replacementLesson.course.id, lessonId: replacementLesson.lesson.id, room: replacementLesson.room, subject: replacementLesson.subject,teacherId: replacementLesson.teacherId, info: replacementLesson.info, date: replacementLesson.date}
+            response.push(dataset);
+        });
+        await res.json(response);
     } catch(e){
         console.log(e);
         res.sendStatus(500)
@@ -186,18 +161,17 @@ router.delete('/id/:id', async function (req,res){
     let id = req.params.id;
     console.log(id)
     try{
-        let replacementLesson: any = await ReplacementLessons.deleteById(id.toString());
-        /*
+        let replacementLesson: ReplacementLesson = await ReplacementLessons.deleteById(id.toString());
+
         let push = new PushNotifications();
-        let devices: any = await User.getStudentDevicesByCourse(replacementLesson);
+        let devices: any = await User.getStudentDevicesByCourse(replacementLesson.course);
+        console.log(devices)
         for (const id in devices) {
             if (devices.hasOwnProperty(id)) {
                 const device = devices[id];
-                await push.send(device.platform, device.device, "Entfernt: " + replacementLesson.subject, "Entfernt: " + replacementLesson.subject + "Datum: " + replacementLesson.date)
+                await push.send(device.platform, device.device, "Entfernt: " + replacementLesson.course.subject, "Entfernt: " + replacementLesson.course.subject + "Datum: " + replacementLesson.date)
             }
         }
-
-         */
         res.sendStatus(200);
     } catch(e){
         console.log(e);
