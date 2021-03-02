c23a6223db501954a34932715fed5a5606830a8e
Nils Witt <nils@nils-witt.de>
2021-03-02 18:01:03 +0100

    updated notifications

--- src/classes/external/PushNotifications.ts
+++ src/classes/external/PushNotifications.ts
@@ -103,6 +103,8 @@ export class PushNotifications {
                         console.log(e)
                         reject();
                     }
+                }else {
+                    resolve();
                 }
             } catch (e) {
                 reject(e);
