fec44f0eb29e8deb6e8440247343f7c49b2432a3
Nils Witt <nils@nils-witt.de>
2021-03-03 21:57:27 +0100

    updated file structure

--- src/classes/Logger.ts
+++ src/classes/Logger.ts
@@ -11,24 +11,8 @@ import winston, {format, transports} from "winston";
 import {ApiGlobal} from "../types/global";
 import 'winston-daily-rotate-file';
 
-const {combine, timestamp, printf} = format;
-
-//Create all 6 hours a new file
-const rotateFile = new (winston.transports.DailyRotateFile)({
-    filename: 'log/%DATE%.log',
-    datePattern: 'YYYY-MM-DD-HH',
-    maxSize: '20m',
-    level: 'silly',
-    format: winston.format.json(),
-    frequency: '6h'
-});
-
-//Logger output format
-const myFormat = printf(({level, message, label, timestamp: timestamp}) => {
-    return `${timestamp} [${label}] ${level}: ${message}`;
-});
-
 declare const global: ApiGlobal;
+
 export class Logger {
 
     /**
@@ -46,5 +30,21 @@ export class Logger {
             ]
         });
     }
-
 }
+
+const {combine, timestamp, printf} = format;
+
+//Create all 6 hours a new file
+const rotateFile = new (winston.transports.DailyRotateFile)({
+    filename: 'log/%DATE%.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    maxSize: '20m',
+    level: 'silly',
+    format: winston.format.json(),
+    frequency: '6h'
+});
+
+//Logger output format
+const myFormat = printf(({level, message, label, timestamp: timestamp}) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
